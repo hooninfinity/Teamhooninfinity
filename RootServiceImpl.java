6aa1f06364b0daeea9a145dc37a3a576a8ba3a84
num5268 <num5268@gmail.com>
2021-04-22 13:47:04 +0900

    Service클래스생성

--- RootServiceImpl.java
+++ RootServiceImpl.java
@@ -1,9 +1,5 @@
-public interface RootService{
-	
-	public void make();
-	public void get();
-	public void set();
-	public void viwe();
-	public void 점수계산();
-}
+public class RootServiceImpl implements RootService{
 
+	// 주저리 주저리
+
+}
